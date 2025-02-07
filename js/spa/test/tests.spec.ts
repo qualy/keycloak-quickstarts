89297fe7222153dda34a26bcbec2d133e7312adc
Akira Takano <a-takano@mail.sorimachi.co.jp>
2025-02-07 21:22:21 +0900

    test

--- js/spa/test/tests.spec.ts
+++ js/spa/test/tests.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('Login', async ({ page }) => {
-  await page.goto('http://localhost:8080/');
+  await page.goto('https://keycloak-quickstarts-zeta.vercel.app/');
 
   await login(page)
 
@@ -9,14 +9,14 @@ test('Login', async ({ page }) => {
 });
 
 test('Logout', async ({ page }) => {
-  await page.goto('http://localhost:8080/');
+  await page.goto('https://keycloak-quickstarts-zeta.vercel.app/');
 
   await login(page)
   await logout(page);
 });
 
 test('Show Access Token', async ({ page }) => {
-  await page.goto('http://localhost:8080/');
+  await page.goto('https://keycloak-quickstarts-zeta.vercel.app/');
 
   await login(page)
 
@@ -25,7 +25,7 @@ test('Show Access Token', async ({ page }) => {
 });
 
 test('Show My Account', async ({ page }) => {
-  await page.goto('http://localhost:8080/');
+  await page.goto('https://keycloak-quickstarts-zeta.vercel.app/');
 
   await login(page)
 
